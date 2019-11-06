e86122f7fe5a155d60b8af949849343256868a29
mishraadityashankar <adityashankarmishra3@gmail.com>
2019-11-06 23:55:55 +0530

    photochanged cv ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,9 +14,9 @@ class Home extends Component {
 					   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 					   				<div class="slider-text-inner js-fullheight">
 					   					<div class="desc">
-						   					<h1>Hi! <br/>I'm Jackson</h1>
-						   					<h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
-												<p><a class="btn btn-primary btn-learn">Download CV <i class="icon-download4"></i></a></p>
+						   					<h1>Hi! <br/>I'm Aditya</h1>
+						   					{/* <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2> */}
+												<p><a class="btn btn-primary btn-learn" href="https://doc-10-cc-docs.googleusercontent.com/docs/securesc/c787oikv36k0pjm77oa862hm60r9r1uj/tfqfsp37vhr4l0kfnckhnf0ki5i56pjf/1573056000000/09444059925293350275/09444059925293350275/13H-huE7kE2vTTy00BCurKiwGsnwKbIui?e=download&nonce=81s4pvs9216eu&user=09444059925293350275&hash=ftiah1k99vduivau6ebvrdf32kkkcisa" download>Download CV <i class="icon-download4"></i></a></p>
 											</div>
 					   				</div>
 					   			</div>
